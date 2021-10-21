6c5f645f682c75aa77830d9e47de926805115ab5
Chris-Pagan <christopher.pagan@outlook.com>
2021-10-21 08:58:18 -0700

    init commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import "./styles.css";
+
+/**
+  Write the addheadings() function here
+*/
+const articles = document.querySelector("article")
+
+
+/**
+  Write the styleTutorialsAndArticles() function here
+*/
+
+/**
+  Write the separateAllTutorials() function here
+*/
+
+/**
+  No need to edit the following
+*/
+function main() {
+  addHeadings();
+  styleTutorialsAndArticles();
+  separateAllTutorials();
+}
+
+main();
